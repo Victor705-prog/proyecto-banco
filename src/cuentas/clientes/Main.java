aab72bc0d783ad30f4997833db1d0806a2e096bc
Victor <victorfernandez574@gmail.com>
2025-02-28 14:39:27 +0100

    cuentas clientes

---
+++ src/cuentas/clientes/Main.java
@@ -0,0 +1,31 @@
+package cuentas.clientes;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cuenta cuenta1;
+        double saldoActual;
+
+        cuenta1 = new Cuenta("Rafael Nadal Parera","1000-2365-85-1230456789",2500,0);
+        
+        operativa_cuenta(cuenta1, 2300);
+    }
+
+    private static void operativa_cuenta(Cuenta cuenta1, float cantidad) {
+        System.out.println("El saldo actual es:"+ cuenta1.estado() );
+        
+        try {
+            cuenta1.retirar(2300);
+            System.out.println("El saldo actual es:"+ cuenta1.estado() );
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            cuenta1.ingresar(695);
+            System.out.println("El saldo actual es:"+ cuenta1.estado() );
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+}
